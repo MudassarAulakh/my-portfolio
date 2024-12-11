133d1640077988563292ca68db2dbe54be585a28
Mudassar Aulakh <mudassarhussain4047769@gmail.com>
2024-12-11 23:27:05 +0500

    links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,31 +14,32 @@ function Footer() {
               rel="noopener noreferrer"
               className="flex items-center justify-center  border-2 border-transparent hover:border-blue-500 p-2 rounded-full transition duration-300"
             >
-              <FaFacebook size={24} className="text-white hover:text-blue-500" />
+              <FaFacebook
+                size={24}
+                className="text-white hover:text-blue-500"
+              />
             </a>
-            {/* <a
-              href="https://www.youtube.com/@Code_Sir"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center justify-center border-2 border-transparent hover:border-red-600 p-2 rounded-full transition duration-300"
-            >
-              <IoLogoYoutube size={24} className="text-white hover:text-red-600" />
-            </a> */}
             <a
-              href="https://www.instagram.com/muzammil.sial.142?igsh=YzljYTk1ODg3Zg=="
+              href="https://www.instagram.com/ma_hu_dev/"
               target="_blank"
               rel="noopener noreferrer"
               className="flex items-center justify-center border-2 border-transparent hover:border-pink-500 p-2 rounded-full transition duration-300"
             >
-              <FaInstagram size={24} className="text-white hover:text-pink-500" />
+              <FaInstagram
+                size={24}
+                className="text-white hover:text-pink-500"
+              />
             </a>
             <a
-              href="https://www.linkedin.com/in/muzammil-hussain-khan-sial-879408332?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+              href="https://www.linkedin.com/in/mudassar-aulakh-b60256290/"
               target="_blank"
               rel="noopener noreferrer"
               className="flex items-center justify-center border-2 border-transparent hover:border-blue-600 p-2 rounded-full transition duration-300"
             >
-              <FaLinkedinIn size={24} className="text-white hover:text-blue-600" />
+              <FaLinkedinIn
+                size={24}
+                className="text-white hover:text-blue-600"
+              />
             </a>
           </div>
           <hr className="bg-white text-white border-[0.3px] opacity-5 mt-5 border-blue-50 w-full" />
@@ -46,7 +47,7 @@ function Footer() {
             <p className="text-sm text-white">
               &copy; 2024 Your Company. All rights reserved.
             </p>
-            <p className="text-sm text-white">Supportive Partner ❤️ coding sir</p>
+            <p className="text-sm text-white">Mudassar Aulakh</p>
           </div>
         </div>
       </div>
